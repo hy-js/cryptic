c156e1422105e08e054f027b3f005e6c78fc98d9
RHYS <62794614+hy-js@users.noreply.github.com>
2022-08-12 00:41:43 +1000

    apply git stash

--- src/pages/puzzle/[year]/[month]/[day].tsx
+++ src/pages/puzzle/[year]/[month]/[day].tsx
@@ -1,66 +1,99 @@
 import type { NextPage } from "next"
-import Header from "@/components/Header"
 import { useState } from "react"
-import { useEffect } from "react"
-import getDate from "@/utils/getDate"
+import { useRouter } from "next/router"
+import Link from "next/link"
+import { useQuery } from "@tanstack/react-query"
+import axios from "axios"
 
 const Today: NextPage = () => {
-  const { dayNumber } = getDate()
+  const router = useRouter()
+  const { year, month, day } = router.query
+
   const [message, setMessage] = useState("")
   const [answer, setAnswer] = useState("")
-  const [win, setWin] = useState(false)
-  const [data, setData] = useState(null)
-  const [isLoading, setLoading] = useState(false)
-
-  useEffect(() => {
-    setLoading(true)
-    fetch(`/api/puzzle/read/${dayNumber}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data)
-        setAnswer(data.answer)
-        setLoading(false)
-      })
-  }, [])
-
-  const handleChange = (event) => {
-    setMessage(event.target.value)
-    if (message.toUpperCase() === answer) {
-      setWin(true)
-      setMessage("")
+  const { data, isLoading, isError, refetch } = useQuery(
+    ["puzzle"],
+    async () => {
+      return await axios
+        .post(`/api/puzzle/`, { month, day })
+        .then((res) => res.data)
     }
+  )
+  const handleClick = () => {
+    // manually refetch
+    refetch()
   }
 
   return (
-    <main className='animation:fade-in container mx-auto flex flex-col items-center min-h-screen p-4'>
-      <Header />
+    <>
+      <div className='flex'>
+        <Link
+          href={
+            router.isReady
+              ? `/puzzle/${year}/${month}/${
+                  Number(day) == 1 ? Number(day) : Number(day) - 1
+                }`
+              : "/"
+          }>
+          <button onClick={handleClick}>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              className='h-6 w-6'
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke='currentColor'
+              strokeWidth={2}>
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M15 19l-7-7 7-7'
+              />
+            </svg>
+          </button>
+        </Link>
+        {data?.source}
+        {/* <Link
+          href={
+            router.isReady
+              ? `/puzzle/${year}/${month}/${
+                  Number(day) == 1 ? Number(day) + 30 : Number(day) - 1
+                }`
+              : "/"
+          }>
+          <button onClick={handleClick}>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              className='h-6 w-6'
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke='currentColor'
+              strokeWidth={2}>
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M9 5l7 7-7 7'
+              />
+            </svg>
+          </button>
+        </Link> */}
+      </div>
       <div className='flex flex-col items-stretch'>
         {isLoading && <p>Loading...</p>}
-        {!isLoading && !data ? <p>No cryptic today</p> : <h2>{data?.clue}</h2>}
+        {!isLoading && !data ? (
+          <p>No cryptic today</p>
+        ) : (
+          <h2 className='text-xl md:text-[1.25rem]'>{data?.clue}</h2>
+        )}
         <input
           type='text'
           className='border border-gray-400 rounded-lg p-2'
           id='message'
           name='message'
-          onChange={handleChange}
+          // onChange={handleChange}
           value={message}
         />
       </div>
-      {win && (
-        <>
-          <h3 className='text-purple-300'>You win!</h3>
-          <h4 className='text-purple-300'>{data?.description}</h4>
-        </>
-      )}
-      {/* <div className='flex justify-between items-stretch w-1/3'>
-        <button>
-          <h3 className='underline'> Back</h3>
-        </button>
-        <button>
-          <h3 className='underline'>Next</h3>
-        </button>
-      </div> */}
-    </main>
+    </>
   )
 }
 
