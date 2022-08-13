42131ce474bbce443ec5ee6b3334b6183bb26ff6
RHYS <62794614+hy-js@users.noreply.github.com>
2022-08-13 18:49:43 +1000

    add random title

--- src/components/Navbar/Date.tsx
+++ src/components/Navbar/Date.tsx
@@ -1,14 +1,20 @@
-import React from "react"
 import getDate from "@/utils/getDate"
+import { useRouter } from "next/router"
 
 const Date = () => {
   const { currentYear, currentMonth, currentDay } = getDate()
-
+  const router = useRouter()
   return (
     <div className='navbar flex w-min-screen items-center justify-center bg-neutral-200'>
-      <h2 className='text-lg text-[1rem] leading-normal font-extrabold'>
-        {currentDay} / {currentMonth} / {currentYear}
-      </h2>
+      {router.asPath.split("/")[2] === "today" ? (
+        <h2 className='text-lg text-[1rem] leading-normal font-extrabold'>
+          {currentDay} / {currentMonth} / {currentYear}
+        </h2>
+      ) : (
+        <h2 className='text-lg text-[1rem] leading-normal font-extrabold'>
+          Random
+        </h2>
+      )}
     </div>
   )
 }
