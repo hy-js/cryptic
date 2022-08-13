0e4fff3c35f9f76d49a690dd8fe33919f5479464
RHYS <62794614+hy-js@users.noreply.github.com>
2022-08-13 22:06:32 +1000

    update SEO tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,29 +1,35 @@
 import Footer from "@/components/Footer"
 import { Html, Head, Main, NextScript } from "next/document"
-
+import { NextSeo } from "next-seo"
 export default function Document() {
   return (
     <Html className='antialiased'>
       <Head>
-        <meta charSet='utf-8' />
-        <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
-
-        <meta
-          name='description'
-          content={`Cryptixle - a cryptic crossword game`}
+        <NextSeo
+          title='Cryptixle'
+          description='A Cryptic Crossword Clue Game.'
+          canonical='https://cryptixle.vercel.app/'
+          openGraph={{
+            url: "https://cryptixle.vercel.app/",
+            title: "Cryptixle",
+            description: "A Cryptic Crossword Clue Game.",
+            images: [
+              {
+                url: "/cryptixle.png",
+                width: 800,
+                height: 600,
+                alt: "Cryptixle Game",
+                type: "image/jpeg"
+              }
+            ],
+            site_name: "Cryptixle"
+          }}
+          twitter={{
+            handle: "@hyjs_",
+            site: "https://cryptixle.vercel.app/",
+            cardType: "summary_large_image"
+          }}
         />
-        <meta
-          name='keywords'
-          content='cryptic, crossword, wordle, puzzle, game'
-        />
-        <link
-          href='/icons/icon-32.png'
-          rel='icon'
-          type='image/png'
-          sizes='32x32'
-        />
-        <link rel='apple-touch-icon' href='/icons/icon-192.png'></link>
-        <meta name='theme-color' content='#fafafa' />
       </Head>
       <body className='flex flex-col h-screen'>
         <div className='flex-grow flex-shrink-0 basis-auto'>
